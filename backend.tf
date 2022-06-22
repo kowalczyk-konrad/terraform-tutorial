c9023868704a5850e007b10d487af2ab5bd88ede
kowalczyk.konrad <kowalczyk.konrad@gmail.com>
2022-06-22 19:33:10 +0200

    Initial commit

---
+++ backend.tf
@@ -0,0 +1,19 @@
+terraform {
+/*
+  cloud {
+    organization = "tutorial77"
+
+    workspaces {
+      name = "tutorial"
+    }
+  }
+*/
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
